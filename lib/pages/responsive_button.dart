764dfdd44698dc2b98c6102d6f09c7159c546b9b
fidelis <fidelisnwabisi@gmail.com>
2023-07-05 21:35:50 +0100

    added ResponsiveButton features added dots

--- lib/pages/responsive_button.dart
+++ lib/pages/responsive_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/misc/colors.dart';
 
 // ignore: must_be_immutable
 class ResponsiveButton extends StatelessWidget {
@@ -12,6 +13,17 @@ class ResponsiveButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: width,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: AppColors.mainColor,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Image.asset("img/button-one.png")],
+      ),
+    );
   }
 }
--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -4,6 +4,8 @@ import 'package:travel_app/misc/colors.dart';
 import 'package:travel_app/widgets/app_large_text.dart';
 import 'package:travel_app/widgets/app_text.dart';
 
+import 'responsive_button.dart';
+
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
 
@@ -44,8 +46,9 @@ class _WelcomePageState extends State<WelcomePage> {
               ),
             ),
             child: Container(
-              margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+              margin: const EdgeInsets.only(top: 100, left: 20, right: 20),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -60,8 +63,26 @@ class _WelcomePageState extends State<WelcomePage> {
                           text: text3[index],
                           size: 16,
                         ),
+                      ),
+                      const SizedBox(height: 40),
+                      ResponsiveButton(
+                        width: 120,
                       )
                     ],
+                  ),
+                  Column(
+                    children: List.generate(3, (indexDots) {
+                      return Container(
+                        margin: const EdgeInsets.only(bottom: 2),
+                        width: 8,
+                        height: index == indexDots ? 25 : 8,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: index == indexDots
+                                ? AppColors.mainColor
+                                : AppColors.mainColor.withOpacity(0.3)),
+                      );
+                    }),
                   )
                 ],
               ),
