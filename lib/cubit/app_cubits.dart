c25329b9dd02a15054f011c3dca0aa120025e446
fidelis <fidelisnwabisi@gmail.com>
2023-07-09 00:53:03 +0100

    added bloc, flutter_bloc, and equatable

---
+++ lib/cubit/app_cubits.dart
@@ -0,0 +1,7 @@
+// Take the persona of [Expert Persona].
+// [Verb][format and length][objective]
+// The output should include relevant [Data]
+// the writing style is
+// [Tone of voice]
+// targeted toward
+// [Audience]
