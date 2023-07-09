264b1b4a13083e2b77727f760713ef7e12d8992d
fidelis <fidelisnwabisi@gmail.com>
2023-07-09 19:08:27 +0100

    added http package, app_cubit_logic.dart, app_cubit_state.dart and app_cubit.dart

--- lib/cubit/app_cubits.dart
+++ lib/cubit/app_cubits.dart
@@ -1,3 +1,12 @@
+import 'package:bloc/bloc.dart';
+import 'package:travel_app/cubit/app_cubit_states.dart';
+
+class AppCubits extends Cubit<CubitStates> {
+  AppCubits() : super(InitialState()) {
+    emit(WelcomeState());
+  }
+}
+
 // Take the persona of [Expert Persona].
 // [Verb][format and length][objective]
 // The output should include relevant [Data]
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_app/cubit/app_cubit_logics.dart';
+import 'package:travel_app/cubit/app_cubits.dart';
 import 'package:travel_app/pages/detail_page.dart';
 import 'package:travel_app/pages/home_page.dart';
 import 'package:travel_app/pages/nav_pages/main_page.dart';
@@ -19,25 +22,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailPage(),
+      home: BlocProvider<AppCubits>(
+        create: (context) => AppCubits(),
+        child: AppCubitLogics(),
+      ),
     );
   }
 }
